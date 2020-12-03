f98fe6e30bbbf6191480f1e3d407cf6d2adc3d58
Ronaldo Junior <ronaldocosta2603@gmail.com>
2020-12-03 10:45:52 -0300

    Genre Service and Resource

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	public List<Movie> findAll(){
+		return null;
+	}
+}
