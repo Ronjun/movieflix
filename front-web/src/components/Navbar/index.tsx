fdda4a3d90e56cf702695aee8bcc1d232cc38237
Ronaldo Junior <ronaldocosta2603@gmail.com>
2021-01-22 09:51:30 -0300

    Navbar and initial config

---
+++ front-web/src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+import { ReactComponent as Logo } from '../../assets/popcorn.svg';
+
+export default function Navbar() {
+  return(
+    <nav className="nav-container">
+      <Logo className='nav-logo'/>
+      <h1 className="nav-text">MovieFlix</h1>
+    </nav>
+  )
+}
